f80dd7aeb425bdef320f02aabe7b10d84a95ba17
hasnaazaki123 <76954101+hasnaazaki123@users.noreply.github.com>
2022-02-26 22:07:14 +0200

    Add files via upload

--- hasnaa-common/main.dart
+++ hasnaa-common/main.dart
@@ -1,25 +1,33 @@
 import 'package:flutter/material.dart';
-import 'package:metrotic/screens/home_screen.dart';
-import 'package:metrotic/screens/splash_screen.dart';
-import 'package:metrotic/screens/welcome_screen.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
+
 void main() {
-  runApp( MyApp());
+  //int y=DateTime. now().toUtc().millisecondsSinceEpoch;
+   // print(y);
+  
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  //const MyApp({Key? key}) : super(key: key);
+ String name ='hasnaa' ;
+ String origin_station = 'shoubra';
+ String destination_station = 'elbhoos';
+ int x=DateTime. now().toUtc().millisecondsSinceEpoch;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'METRO',
-     initialRoute: '/', 
-      routes: {
-       // '/' : (ctx)=>welcomeScreen(),
-       '/' : (ctx)=>HomeScreen(),
-      //'/':(ctx)=>SplashScreen(),
-      },    
+      home: Scaffold(
+          body: Container(
+        child: QrImage(
+          data: "$name$origin_station$destination_station$x",
+          version: QrVersions.auto,
+          size: 200.0,
+          padding: EdgeInsets.all(40),
+        ),
+
+      )),
     );
   }
 }
-
-
-
